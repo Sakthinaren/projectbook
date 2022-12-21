653bb0a3ab1dc46f8db281362c4b085b46cf57ac
sakthinarensak <sakthinaren06@gmail.com>
2022-12-21 20:22:51 +0530

    sakthi code

--- src/test/java/example/Pro.java
+++ src/test/java/example/Pro.java
@@ -4,6 +4,8 @@ public class Pro {
 	public static void main(String[] args) {
 		System.out.println("sakthi");
 		System.out.println("priya");
+		System.out.println("changes made");
+		System.out.println("new update");
 	}
 	
 
